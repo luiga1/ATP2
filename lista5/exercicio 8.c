0c345ab19b9e506f8865df67e2a787c92f6f28e1
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2025-08-20 17:26:02 -0300

    Update exercicio 8.c

--- lista5/exercicio 8.c
+++ lista5/exercicio 8.c
@@ -5,33 +5,28 @@
 depois procura novamente outro divisor comum, e multiplica ele pelo divisor anterior
 até que não tenha mais um divisor entre eles*/
 
-int Mdc(int primeiro_numero, int segundo_numero, int divisor);
-
-int main(){
-    int numero1, numero2, divisor;
-    
-    printf("Digite o primeiro numero: ");
-    scanf("%d",&numero1);
-    printf("Digite o segundo numero: ");
-    scanf("%d",&numero2);
-    divisor = Mdc(numero1,numero2,2);
-    printf("o MDC é %d\n",divisor);
-    
-
-return 0;
-}
-
-int Mdc(int primeiro_numero, int segundo_numero, int divisor){
-    for(int i = divisor; i>primeiro_numero|| i>segundo_numero; i++){ //procura o proximo divisor até o menor numero
-        printf("pass");
+int Mdc(int primeiro_numero, int segundo_numero, int divisor, int n){ 
+  if(primeiro_numero == 1 || segundo_numero == 1) return n;
+    for(;divisor<= primeiro_numero|| divisor<= segundo_numero; divisor++){ //procura o proximo divisor até o menor numero
         if((primeiro_numero%divisor ==0) && (segundo_numero%divisor ==0)){
             primeiro_numero = primeiro_numero / divisor;
             segundo_numero = segundo_numero / divisor;
-        return divisor * (primeiro_numero, segundo_numero,2); // multiplicar os menores divisores
-    }else{
-        divisor++;
-        printf("divisor++");
+            n = n * divisor;
+         return n = Mdc(primeiro_numero, segundo_numero,2,n); // multiplicar os menores divisores
     }
+    }
+  return n;
 }
-return 1;
+
+int main(){
+    int numero1, numero2,
+     divisor;
+
+  numero1 = 24;
+  numero2 = 120;
+
+    divisor = Mdc(numero1,numero2,2,1);
+    printf("o MDC é %d\n",divisor);
+
+return 0;
 }
