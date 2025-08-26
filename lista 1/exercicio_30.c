0dade8f31a922a33cea2ac0bf0acebf9ed52c9c5
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2025-08-26 15:38:06 -0300

    Update exercicio_30.c

--- lista 1/exercicio_30.c
+++ lista 1/exercicio_30.c
@@ -1,12 +1,13 @@
 /*use um array de ponteiros para armazenar 3 strings, ordene as strings em ordem alfabetica e as imprima.*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define STR_SIZE 3
 #define SIZE 100
 
 int main(){
-    int i;
-    char *ponteiros[STR_SIZE],*aux[SIZE];
+    int i,j;
+    char *ponteiros[STR_SIZE];
 
     for(i=0;i<STR_SIZE;i++) ponteiros[i] = malloc(sizeof(char)*SIZE);
 
@@ -14,15 +15,20 @@ int main(){
         printf("Digite a %d palavra ",i+1);
         scanf("%s", ponteiros[i]);
     }
-    for(i=1;i<STR_SIZE+1;i++){
-        if( strcomp(ponteiros[i-1],ponteiros[i]) < 0 ){
-            //i-1 pro aux, i pro i-1, e auxi pro i
-            aux = ponteiros[i-1];
-            ponteiro[i-1] = ponteiro[i];
-            ponteiro[i] = aux;
+
+    for(i = 0; i < STR_SIZE - 1; i++) {
+        for(j = i + 1; j < STR_SIZE; j++) {
+            if(strcmp(ponteiros[i], ponteiros[j]) > 0) {
+                char *aux = ponteiros[i];
+                ponteiros[i] = ponteiros[j];
+                ponteiros[j] = aux;
+            }
         }
     }
-
+    printf("\nStrings em ordem alfabetica:\n");
+    for(i = 0; i < STR_SIZE; i++) {
+        printf("%s\n", ponteiros[i]);
+        free(ponteiros[i]);
+    }
 return 0;
 }
-
