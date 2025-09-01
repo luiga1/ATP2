dc2555f56f5fdab6b3135e17b6c40ba9d77d6876
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2025-09-01 15:23:03 -0300

    Update exercicio_15.c

--- lista5/exercicio_15.c
+++ lista5/exercicio_15.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void subconjuntos(int* array);
+void subconjuntos(int* array, int i, int size, int *subc, int sub_len);
 
 int main(){
-    int tamanho, *conjunto;
+    int tamanho, sub,
+     *conjunto, *subconj;
 
     printf("digite o tamanho do conjunto ");
     scanf("%d",&tamanho);
 
     conjunto = malloc(sizeof(int) * tamanho);
+    subconj = malloc(sizeof(int) * tamanho);
 
     for(int i=0;i<tamanho;i++) *(conjunto +i) = i+1;
 
-    subconjuntos(conjunto);
+    subconjuntos(conjunto, 0, tamanho, subconj, sub);
 
 }
 
-void subconjuntos(int* array){
-    
+void subconjuntos(int* array, int i, int size, int *subc, int sub_len){
+    if(i == size){
+        printf("{");
+
+        for(int j=0; j<sub_len;j++){
+            if(j== sub_len -1){
+                printf("%d", *(subc +j));
+            }else{
+                printf("%d ,",*(subc+j));
+            }
+        }
+        printf("}\n");
+        return;
+    }
+    *(subc + sub_len) = *(array + i);
+
+    subconjuntos(array, i+1, size, subc, sub_len +1 );
+
+    subconjuntos(array, i+1, size, subc, sub_len);
+
 }
