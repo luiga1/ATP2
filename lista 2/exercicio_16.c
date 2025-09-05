1caa1592bc1dda9b6101f75bc9ca2819936cff52
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2025-09-04 21:27:33 -0300

    Update exercicio_16.c

--- lista 2/exercicio_16.c
+++ lista 2/exercicio_16.c
@@ -4,19 +4,23 @@
 #define COL_SIZE 5
 
 void print_matriz(int matriz[LIN_SIZE][COL_SIZE]);
-int primo(int n, int divisor, int contador);
+int primo(int n);
 
 int main(){
     int matriz[LIN_SIZE][COL_SIZE], k=1;
 
     for(int i=0;i<LIN_SIZE;i++){
         for(int j=0;j<COL_SIZE;j++){
-            matriz[i][j] = primo(2, 2, i);
+            while(primo(k) == 0){
+                k++;
+            }
+            matriz[i][j] = k;
+            k++;
         }
     }
 
     printf("A matriz de numeros primos e \n");
-    print_matriz(matriz[LIN_SIZE][COL_SIZE]);
+    print_matriz(matriz);
 
 return 0;
 }
@@ -26,32 +30,16 @@ void print_matriz(int matriz[LIN_SIZE][COL_SIZE]){
     for(int i=0;i<LIN_SIZE;i++){
             printf("\n");
         for(int j=0;j<COL_SIZE;j++){
-            printf (" %d", matriz[i][j]);
+            printf (" %0.2d", matriz[i][j]);
         }
     }
      printf("\n\n");
 }
 
-int primo(int n, int divisor, int contador){
-/*
-- menor ou igual a dois
- n que vai ser comparada com um divisor até chegar n == divisor e almenta 1 no contador
- se n % divisor == 0 não é primo, e pula para o proximo n;
-*/
-  if(contador == 0) return;
-  if(n == divisor){
-    return n;
-    contador--;
-    n++;
-     return primo(n, 2, contador); // divisor reseta acada novo numero
-
-}
-  if(n%divisor != 0) {
-    divisor++;
-     return primo(n, divisor, contador);
-  }
-  else{
-    n++;
-    return primo(n, 2, contador);
-  }
+int primo(int n){
+    if(n<2) return 0;
+    for(int i=2; i < n; i++){
+        if(n % i == 0) return 0;
+    }
+    return 1;
 }
