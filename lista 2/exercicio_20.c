e2949651aa9d4f89b5a4af6b394f4694dea8751e
Luiga_luis <81785460+luiga1@users.noreply.github.com>
2025-09-05 15:59:37 -0300

    Update exercicio_20.c

--- lista 2/exercicio_20.c
+++ lista 2/exercicio_20.c
@@ -8,16 +8,50 @@
 void print_matriz(int linha, int coluna, int** matriz);
 void cria_matriz_rand(int linha, int coluna, int** matriz);
 int** aloca_matriz(int linha, int coluna, int**matriz);
-void soma_coluna(int linha, int coluna, int** matriz);
+int soma_coluna(int linha, int coluna, int** matriz);
 
 int main(){
-    int **matriz;
+    int **matriz,
+    *somas;
 
     matriz = aloca_matriz(LIN_SIZE, COL_SIZE,matriz);
+    somas = malloc(sizeof(int)*4);
+
+
+
+    for(int i=0;i<4;i++){
+        somas[i] = soma_coluna(LIN_SIZE,i, matriz);
+    }
+
+    for(int i=0; i<4;i++){
+        printf("a coluna %d vale %d\n",i+1,somas[i]);
+    }
 
 return 0;
 }
 
-// linha passa  soma o elemento da coluna e pula pra proxima linha
+void cria_matriz_rand(int linha, int coluna, int** matriz){
+    srand(time(0));
+    for(int i=0;i<linha;i++){
+        for(int j=0;j<coluna;j++){
+            matriz[i][j] = rand()%6;
+        }
+    }
+}
 
-// terminar
+int** aloca_matriz(int linha, int coluna, int**matriz){
+    matriz = malloc(sizeof(int*)*linha);
+    for(int i=0;i<linha;i++){
+        *(matriz + i) = malloc(sizeof(int)*coluna);
+    }
+    return matriz;
+}
+
+int soma_coluna(int linha, int coluna, int** matriz){
+    int soma=0;
+        for(int j=0;j<linha;j++){
+            soma += matriz[j][coluna];
+        }
+
+    return soma;
+}
