64f21b7dea1391fede8fb1df3880f17e72c88d24
Luis Miguel <luis.miguel.flauzino@gmail.com>
2025-10-26 11:52:25 -0300

    exercicios de lista ligada

---
+++ exercicios extras/lista_ligada.c
@@ -0,0 +1,20 @@
+/*exercicio extra para entender listas ligadas*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Estrutura da lista ligada -> o conteurod e um ponteiro para o proximo struct*/
+typedef struct ligada{
+    int num;
+    int* prox;
+} Numero;
+
+int main(){
+    Numero cabeça; // define aonde começa a lista 
+
+    
+
+    //novo numero recebe o endereço da cabeça e a cabeça recebe o endereco do novo numero
+
+    return 0;
+}
+
