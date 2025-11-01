bf9f16db2624d39c6566cc373295a48cd271f86c
Luis Miguel <luis.miguel.flauzino@gmail.com>
2025-11-01 16:23:25 -0300

    exercicio 10 e 11

--- lista4/exercicio_10.c
+++ lista4/exercicio_10.c
@@ -1,3 +1,28 @@
 /*Escreva um programa em C para copiar o conteudo de um arquivo binario para outro arquivo.*/
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(){
+    FILE* arq, *copy;
+    char c;
+
+    arq = fopen("arquivob1.bin","rb");
+    copy = fopen("arquivob2.bin","wb");
+
+    if(arq == NULL){
+        perror("arquivob1");
+        return 1;
+    }
+    if(copy == NULL){
+        perror("arquivob2");
+    }
+
+    while((c = fgetc(arq)) != EOF){
+        fputc(c,copy);
+    }
+
+    fclose(arq);
+    fclose(copy);
+
+    return 0;
+}
---
+++ lista4/exercicio_11.c
@@ -0,0 +1,2 @@
+/*Escreva um programa em C para ler e exibir um registro especifico em umarquivo binario.*/
+
